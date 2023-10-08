f29a3ab8fb843f758ba1b45069bb10c38eb8fd9f
Vefery <easygamer751@gmail.com>
2023-10-08 13:12:53 +0700

    Initial commit

---
+++ Lab2/Encrypter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Encrypter
+    {
+        readonly static string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        public static string EncryptMessage(string message, string key)
+        {
+            message = message.ToLower();
+            string encryptedMessage = "";
+            int index;
+            for (int i = 0; i < message.Length; i++) 
+            {
+                if ((index = alphabet.IndexOf(message[i])) < 0)
+                    continue;
+                encryptedMessage += key[index];
+            }
+            return encryptedMessage;
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Lab2;
+
+class Program
+{
+
+    public static void Main()
+    {
+        string shuffledAlphabet = AlphabetShuffler.GetShuffledAlphabet();
+        string message;
+        Console.WriteLine(shuffledAlphabet);
+        message = Console.ReadLine();
+        Console.WriteLine(Encrypter.EncryptMessage(message, shuffledAlphabet));
+    }
+}
